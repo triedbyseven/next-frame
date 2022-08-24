4f53706d999dc91c8d4cf196d844c9ae73814395
Michael Camacho <mikedev0431@gmail.com>
2022-08-24 15:49:52 -0700

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body { 
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+interface ThemeInterface {
+  colors: {
+    primary: string
+  }
+};
+
+const theme: ThemeInterface = {
+  colors: {
+    primary: '#0070f3',
+  }
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default MyApp
